8ec0a927e6b4cf4c951c1acb459edc169f149224
RuiJ72 <rlagos900@gmail.com>
2022-11-04 00:20:24 -0300

    First commit

---
+++ EjercicioSWITCH/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("\n------------Lenguajes de Programación--------------------------------");
+Console.WriteLine("\n---Qual es tu lenguaje de programación favorito.Introduce el número: ");
+Console.WriteLine("-------------------------1-Javascript----------------------------------");
+Console.WriteLine("-------------------------2-C# .NET-------------------------------------");
+Console.WriteLine("-------------------------3-Python--------------------------------------");
+Console.WriteLine("-------------------------4-Java----------------------------------------");
+Console.WriteLine("-------------------------5-TypeScript----------------------------------");
+Console.WriteLine("-------------------------6-Pascal--------------------------------------");
+Console.WriteLine("-------------------------7-Kotlin--------------------------------------");
+Console.WriteLine("-------------------------8-Otra----------------------------------------");
+
+int escojer = Convert.ToInt32(Console.ReadLine());
+
+
+switch (escojer)
+{
+    case 1:
+
+        Console.WriteLine("Has elegido el lenguaje de programación: Javascript");
+        break;
+    case 2:
+        Console.WriteLine("Has elegido el lenguaje de programación: C# .NET");
+        break;
+    case 3:
+        Console.WriteLine("Has elegido el lenguaje de programación: Python");
+        break;
+    case 4:
+        Console.WriteLine("Has elegido el lenguaje de programación: Java");
+        break;
+    case 5:
+        Console.WriteLine("Has elegido el lenguaje de programación: TypeScript");
+        break;
+    case 6:
+        Console.WriteLine("Has elegido el lenguaje de programación: Pascal");
+        break;
+    case 7:
+        Console.WriteLine("Has elegido el lenguaje de programación: Kotlin");
+        break;
+    case 8:
+    default:
+        Console.WriteLine("Tu elección se basa en un lenguaje de programación que no está en la lista");
+        break;
+}
---
+++ EjerciciosSesion5/Program.cs
@@ -0,0 +1,37 @@
+﻿string nobre_cliente = "";
+string email = "";
+string descuento = "";
+string nome = "";
+int cantidad = 0;
+double precio = 0;
+double perc_descuento = 0;
+double total_descuento = 0;
+double total_bruto = 0;
+
+Console.WriteLine("\nIntroduce tu nombre ");
+nobre_cliente = Console.ReadLine();
+
+Console.WriteLine("\nemail: ");
+email = Console.ReadLine();
+
+Console.WriteLine("\n¿Tienes un cupón de descuento? s/n: ");
+descuento = Console.ReadLine();
+
+Console.Write("\n¿Qué producto deseas?: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("\n¿Qué cantidad quieres?");
+cantidad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nInserta el precio: ");
+precio = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n¿cuál es el porcentaje de descuento?");
+perc_descuento = double.Parse(Console.ReadLine());
+
+
+// Percentage Calculation
+total_bruto = cantidad * precio;
+total_descuento = total_bruto - ((perc_descuento / 100) * total_bruto);
+Console.WriteLine("\nEl total con descuento es " + total_descuento);
+
